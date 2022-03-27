d9f83cdb4aaef8d3f43b0fac7940e456a048bf69
Olaf4tza <73993173+Olaf4tza@users.noreply.github.com>
2022-03-27 05:34:22 +0300

    daaaaaa

--- Backend/models/ESG.js
+++ Backend/models/ESG.js
@@ -5,7 +5,9 @@ const ESGSchema = new mongoose.Schema(
         esg: {type: Array},
         cat: {type: Array},
         img: {type: String},
-        last: {type: Number}
+        last: {type: Number},
+        grade: {type: String},
+        prices: {type: Array}
     },
     {timestamps: true}
 );
